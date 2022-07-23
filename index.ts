0bc3a85663a8a7e862f24f8d93a5c907f9035279
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2022-07-23 14:54:53 +0000

    huh

--- index.ts
+++ index.ts
@@ -4,9 +4,13 @@ const dotenv = require("dotenv").config();
 const sernPrefix = process.env.PREFIX
 const token = process.env.TOKEN
 const client = new Client({
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers]
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers, GatewayIntentBits.MessageContent]
 });
 
 Sern.init({client,sernPrefix,commands : './commands',});
+
+client.on('ready', () => {
+    console.log("logged on!")
+})
 
 client.login(token);
