c54e8ef712a1caffa16d7e3e8ffc47e325a66048
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2022-07-26 21:20:21 +0200

    what I've been working on

--- commands/joke.ts
+++ commands/joke.ts
@@ -1,6 +1,9 @@
 const { commandModule, CommandType } = require('@sern/handler');
-const got = require("got");
+import { EmbedBuilder } from "@discordjs/builders";
 import { publish } from "../src/plugins/publish";
+const random = require("something-random-on-discord").Random
+const got = require("got");
+const axios = require('axios');
 
 export default commandModule({
 	name: 'chiste',
@@ -9,8 +12,9 @@ export default commandModule({
 	description: 'Enseña un chiste en inglés.',
 	alias : ['joke'],
 	execute: async (ctx, args) => {
-		got('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&format=txt').then(res => {
-        ctx.reply(res)
-    });
+		const request = got('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&format=json').then(res => {/*const resconsole = console.log(res);*/ res.data});
+		const requestSetup = request.joke || request.setup
+		const requestDelivery = request.delivery || ""
+		ctx.reply({content: `${requestSetup}\n${requestDelivery}`})
 	},
 });
--- index.ts
+++ index.ts
@@ -6,11 +6,25 @@ const token = process.env.TOKEN
 const client = new Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers, GatewayIntentBits.MessageContent]
 });
+const TLauncherWords = [
+    "T Launcher",
+    "tlauncher",
+    "Tlauncher",
+    "TLauncher",
+    "T launcher",
+    "t launcher"
+]
 
 Sern.init({client,sernPrefix,commands : './commands'});
 
 client.on('ready', () => {
     console.log("logged on!")
+})
+
+client.on('message', message => {
+    if (message.content.startsWith(TLauncherWords)) {
+    	message.reply("Hola!\nTLauncher no se puede utilizar en el servidor oficial de Minecraft debido a la gran vulnerabilidad que nos supone tenerlo no premium.\nSi tienes una cuenta premium, puedes pedir permiso con el comando /mcform.\nGracias\n*Este mensaje es automatizado. Si ha sido un error, siento mucho molestar!*")
+  }
 })
 
 client.login(token);
