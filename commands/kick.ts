2cd06ab8723b1b37577920b57072e685bb5c2ce5
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2022-08-04 21:57:25 +0200

    feat: kick command

---
+++ commands/kick.ts
@@ -0,0 +1,37 @@
+const { commandModule, CommandType } = require('@sern/handler');
+import { publish } from "../src/plugins/publish";
+import { ownerOnly } from "../src/plugins/ownerOnly";
+import { ApplicationCommandOptionType, EmbedBuilder } from 'discord.js'
+
+export default commandModule({
+	name: 'kick',
+    type: CommandType.Both,
+	plugins: [publish(['1000400148289036298', '928018226330337280']), ownerOnly()],
+	description: 'ADMIN: Expulsa usuarios.',
+	options: [{
+		name: 'usuario',
+		description: 'Escribe un usuario.',
+		type: ApplicationCommandOptionType.User,
+		required: true,
+		min_value: 2,
+		max_value: 100
+	},
+	{
+		name: 'razon',
+		description: 'Escribe la razón.',
+		type: ApplicationCommandOptionType.String,
+		required: true
+	}],
+	//alias : [],
+	execute: async (ctx, options, message) => {
+		const userToBan = options[1].getMember('usuario', true);
+		const reason = options[1].get('razon', true).value;
+		const kickEmbed = new EmbedBuilder()
+			.setTitle('Nuevo kick.')
+			.setDescription(`Kick efectuado por <@${ctx.author}>.\nRazón: ${reason}.`);
+		const sendToMods = ctx.client.guilds.cache.get('928018226330337280')!.channels.cache.get('1004118323258208257')
+		await sendToMods.send({content: `Se ha expulsado a ${userToBan}.\nKick efectuado por ${ctx.user} con razón "${reason}."`})
+		userToBan.kick(reason)
+		await ctx.reply({content: 'Expulsado correctamente!'})
+	},
+});
--- index.ts
+++ index.ts
@@ -1,22 +1,13 @@
 import { SernEmitter } from "@sern/handler";
-const { EconomyManager } = require('quick.eco');
 const { Client, GatewayIntentBits } = require("discord.js");
 const { Sern } = require("@sern/handler");
 const dotenv = require("dotenv").config();
 const sernPrefix = process.env.PREFIX
 const token = process.env.TOKEN
-const mongo = process.env.ECONOMY_MONGODB
 const client = new Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers],
     restTimeOffset: 0
 });
-/*const eco = new EconomyManager({
-    adapter: 'mongo',
-    adapterOptions: {
-        collection: 'money', // => Collection Name
-        uri: mongo // => Mongodb uri
-    }
-});*/
 
 Sern.init({client,
     sernPrefix,
