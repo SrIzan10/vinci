6796cf6c2ff121c02c3e3cd65f64f1a7d6b42edf
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2022-08-15 14:59:31 +0200

    feat: radio in a vc!

---
+++ commands/misc/playRadio.ts
@@ -0,0 +1,31 @@
+const { commandModule, CommandType } = require('@sern/handler');
+import { publish } from "../../src/plugins/publish";
+import { ownerOnly } from "../../src/plugins/ownerOnly"
+import { AudioPlayerStatus, createAudioPlayer, createAudioResource, joinVoiceChannel } from "@discordjs/voice";
+import got from "got"
+import axios from "axios";
+/*
+import { publish } from "../../src/plugins/publish";
+import { ownerOnly } from "../../src/plugins/ownerOnly"
+*/
+
+export default commandModule({
+	name: 'radio',
+    type: CommandType.Both,
+	plugins: [publish(['1000400148289036298'])],
+	// , '928018226330337280'
+	description: 'ADMIN: Reproduce la radio',
+	//alias : [],
+	options: [],
+	execute: async (ctx, options) => {
+		const stream = await got.stream("https://flucast-m04-06.flumotion.com/cope/rockfm.mp3")
+		const connection = joinVoiceChannel({adapterCreator: ctx.guild.voiceAdapterCreator,channelId: '1008416059432370346',guildId: '1000400148289036298',selfDeaf: true});
+		const resource = createAudioResource(stream, { inlineVolume: true });
+		const player = createAudioPlayer();
+		connection.subscribe(player)
+		player.play(resource)
+		resource.volume!.setVolume(0.7)
+		/*player.on(AudioPlayerStatus.Playing, () => {});*/
+		ctx.reply({content: 'Reproduciendo radio...', ephemeral: true})
+	},
+});
--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ const sernPrefix = process.env.PREFIX
 const mongoose = require('mongoose');
 const youtube = require('discord-bot-youtube-notifications');
 const client = new Client({
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessageReactions],
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildVoiceStates],
     restTimeOffset: 0
 });
 
@@ -30,6 +30,7 @@ client.on('ready', async () => {
         { name: "cómo escribe Javi", type: ActivityType.Watching },
         { name: "a Hermes", type: ActivityType.Watching },
         { name: "tus comandos", type: ActivityType.Listening },
+        { name: "tu voz", type: ActivityType.Listening }
     ]
         var randomStatus = statuses[Math.floor(Math.random() * statuses.length)];
         client.user.setActivity(randomStatus);
@@ -52,10 +53,8 @@ const Notifier = new youtube.notifier(client, {
     // The youtube data v3 API key, Send this if you want updates to be fast and precise because without the key it take 10-15 minutes more time to get latest videos
     apiKey: process.env.YOUTUBE_API,
 });
-
 const youtube_channel_id = "UC9G2yvrtrPeJFEzwlshg5HA";
 const discord_channel_id = "948690278498320404";
-
 Notifier.addNotifier(youtube_channel_id, discord_channel_id);
 
 client.login(process.env.TOKEN);
