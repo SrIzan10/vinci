b496e3443f55e6398e549f5669bc44853d1460ff
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2022-08-23 23:01:07 +0200

    feat: nuevo sistema de radio :D

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 vinci bot
 ## badges yes sir
-[![CodeFactor](https://www.codefactor.io/repository/github/srizan10/vinci/badge)](https://www.codefactor.io/repository/github/srizan10/vinci)
+[![CodeFactor](https://www.codefactor.io/repository/github/srizan10/vinci/badge)](https://www.codefactor.io/repository/github/srizan10/vinci) [![wakatime](https://wakatime.com/badge/user/4ad16edf-eadc-48d9-b010-26f275fe0be6/project/120bd895-55e3-42fe-894b-bd974f6f7312.svg)](https://wakatime.com/badge/user/4ad16edf-eadc-48d9-b010-26f275fe0be6/project/120bd895-55e3-42fe-894b-bd974f6f7312)
 # heres a roadmap
 
 - ~~form to apply for the minecraft server~~ DONE!
--- commands/misc/playRadio.ts
+++ commands/misc/playRadio.ts
@@ -1,9 +1,10 @@
 const { commandModule, CommandType } = require('@sern/handler');
 import { publish } from "../../src/plugins/publish";
 import { ownerOnly } from "../../src/plugins/ownerOnly"
-import { AudioPlayerStatus, createAudioPlayer, createAudioResource, joinVoiceChannel } from "@discordjs/voice";
+import { createAudioPlayer, createAudioResource, joinVoiceChannel } from "@discordjs/voice";
 import got from "got"
-import axios from "axios";
+import { ApplicationCommandOptionType, EmbedBuilder } from "discord.js";
+const m3u8stream = require("m3u8stream");
 /*
 import { publish } from "../../src/plugins/publish";
 import { ownerOnly } from "../../src/plugins/ownerOnly"
@@ -13,18 +14,105 @@ export default commandModule({
 	name: 'radio',
     type: CommandType.Both,
 	plugins: [publish({ guildIds: ['1000400148289036298', '928018226330337280'] }), ownerOnly()],
-	description: 'ADMIN: Reproduce la radio',
-	//alias : [],
-	options: [],
+	description: 'ADMIN: Reproduce la radio', 
+	options: [
+		{
+			name: 'reproducir',
+			description: 'Reproduce una radio de la lista',
+			type: ApplicationCommandOptionType.String,
+			autocomplete: true,
+			required: true,
+			command: {
+				onEvent: [],
+				async execute(ctx){
+					const focusedValue = ctx.options.getFocused();
+					const choices = ['Rock FM', 'Cadena 100', 'Cadena Dial', 'Europa FM', 'BBC 1', 'BBC 5', 'RNE 1', 'RNE 5', 'Los 40'];
+					const filtered = choices.filter(choice => choice.startsWith(focusedValue));
+					await ctx.respond(
+						filtered.map(choice => ({ name: choice, value: choice })),
+					);
+				}
+			}
+		}
+	],
 	execute: async (ctx, options) => {
-		const stream = await got.stream("https://flucast-m04-06.flumotion.com/cope/rockfm.mp3")
-		const connection = joinVoiceChannel({adapterCreator: ctx.guild.voiceAdapterCreator,channelId: '1008730592835281009',guildId: '928018226330337280',selfDeaf: true});
-		const resource = createAudioResource(stream, { inlineVolume: true });
-		const player = createAudioPlayer();
-		connection.subscribe(player)
-		player.play(resource)
-		resource.volume!.setVolume(0.7)
-		/*player.on(AudioPlayerStatus.Playing, () => {});*/
-		ctx.reply({content: 'Reproduciendo radio...', ephemeral: true})
+		const radioname = options[1].getString("reproducir", true) as string;
+		if (radioname === 'Rock FM') {
+			const stream = await got.stream("https://flucast-m04-06.flumotion.com/cope/rockfm.mp3")
+			const connection = joinVoiceChannel({adapterCreator: ctx.guild.voiceAdapterCreator,channelId: '1008730592835281009',guildId: '928018226330337280',selfDeaf: true});
+			const resource = createAudioResource(stream, { inlineVolume: true });
+			const player = createAudioPlayer();
+			connection.subscribe(player)
+			player.play(resource)
+			resource.volume!.setVolume(0.7)
+		} else if (radioname === 'Cadena 100') {
+			const stream = await got.stream("https://server8.emitironline.com:18196/stream")
+			const connection = joinVoiceChannel({adapterCreator: ctx.guild.voiceAdapterCreator,channelId: '1008730592835281009',guildId: '928018226330337280',selfDeaf: true});
+			const resource = createAudioResource(stream, { inlineVolume: true });
+			const player = createAudioPlayer();
+			connection.subscribe(player)
+			player.play(resource)
+			resource.volume!.setVolume(0.7)
+		} else if (radioname === 'Cadena Dial') {
+			const stream = await got.stream("http://20853.live.streamtheworld.com/CADENADIAL.mp3")
+			const connection = joinVoiceChannel({adapterCreator: ctx.guild.voiceAdapterCreator,channelId: '1008730592835281009',guildId: '928018226330337280',selfDeaf: true});
+			const resource = createAudioResource(stream, { inlineVolume: true });
+			const player = createAudioPlayer();
+			connection.subscribe(player)
+			player.play(resource)
+			resource.volume!.setVolume(0.7)
+		} else if (radioname === 'Europa FM') {
+			const stream = await m3u8stream('https://livefastly-webs.europafm.com/europafm/audio/master.m3u8')
+			const connection = joinVoiceChannel({adapterCreator: ctx.guild.voiceAdapterCreator,channelId: '1008730592835281009',guildId: '928018226330337280',selfDeaf: true});
+			const resource = createAudioResource(stream, { inlineVolume: true });
+			const player = createAudioPlayer();
+			connection.subscribe(player)
+			player.play(resource)
+			resource.volume!.setVolume(0.7)
+		} else if (radioname === 'BBC 1') {
+			const stream = await got.stream("http://stream.live.vc.bbcmedia.co.uk/bbc_radio_one")
+			const connection = joinVoiceChannel({adapterCreator: ctx.guild.voiceAdapterCreator,channelId: '1008730592835281009',guildId: '928018226330337280',selfDeaf: true});
+			const resource = createAudioResource(stream, { inlineVolume: true });
+			const player = createAudioPlayer();
+			connection.subscribe(player)
+			player.play(resource)
+			resource.volume!.setVolume(0.7)
+		} else if (radioname === 'BBC 5') {
+			const stream = await got.stream("https://server8.emitironline.com:18196/stream")
+			const connection = joinVoiceChannel({adapterCreator: ctx.guild.voiceAdapterCreator,channelId: '1008730592835281009',guildId: '928018226330337280',selfDeaf: true});
+			const resource = createAudioResource(stream, { inlineVolume: true });
+			const player = createAudioPlayer();
+			connection.subscribe(player)
+			player.play(resource)
+			resource.volume!.setVolume(0.7)
+		} else if (radioname === 'RNE 1') {
+			const stream = await got.stream("https://crtve-rne1-cnr.cast.addradio.de/crtve/rne1/cnr/mp3/high")
+			const connection = joinVoiceChannel({adapterCreator: ctx.guild.voiceAdapterCreator,channelId: '1008730592835281009',guildId: '928018226330337280',selfDeaf: true});
+			const resource = createAudioResource(stream, { inlineVolume: true });
+			const player = createAudioPlayer();
+			connection.subscribe(player)
+			player.play(resource)
+			resource.volume!.setVolume(0.7)
+		} else if (radioname === 'RNE 5') {
+			const stream = await got.stream("http://crtve--di--crtve-ice--02--cdn.cast.addradio.de/crtve/rne5/sev/mp3/high")
+			const connection = joinVoiceChannel({adapterCreator: ctx.guild.voiceAdapterCreator,channelId: '1008730592835281009',guildId: '928018226330337280',selfDeaf: true});
+			const resource = createAudioResource(stream, { inlineVolume: true });
+			const player = createAudioPlayer();
+			connection.subscribe(player)
+			player.play(resource)
+			resource.volume!.setVolume(0.7)
+		} else if (radioname === 'Los 40') {
+			const stream = await got.stream('http://stream.ondaceronoroeste.es:8000/stream')
+			const connection = joinVoiceChannel({adapterCreator: ctx.guild.voiceAdapterCreator,channelId: '1008730592835281009',guildId: '928018226330337280',selfDeaf: true});
+			const resource = createAudioResource(stream, { inlineVolume: true });
+			const player = createAudioPlayer();
+			connection.subscribe(player)
+			player.play(resource)
+			resource.volume!.setVolume(0.7)
+		}
+		const embed = new EmbedBuilder()
+			.setTitle(`Reproduciendo ${radioname}.`)
+			.setDescription(`A veces la radio tarda en cargar, sé paciente :'D`);
+		ctx.reply({embeds: [embed], ephemeral: true})
 	},
 });
