e16bd026a2d153f8dfd00c6424839c7b88e62772
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2022-08-24 12:10:41 +0200

    feat: new /a command

--- commands/fun/a.ts
+++ commands/fun/a.ts
@@ -1,6 +1,5 @@
 const { commandModule, CommandType } = require('@sern/handler');
 import { publish } from "../../src/plugins/publish";
-const attachment1 = 'A.png';
 
 export default commandModule({
 	name: 'a',
@@ -8,7 +7,20 @@ export default commandModule({
 	plugins: [publish({ guildIds: ['1000400148289036298', '928018226330337280'] })],
 	description: 'A',
 	//alias : [],
-	execute: async (ctx, args) => {
-		await ctx.reply({content: 'A', files: [attachment1]});
+	execute: async (ctx, options) => {
+		const imagesArray = [
+			'./images/XaviXE.png',
+			// 'images/Paula.png',
+			'./images/William.png',
+			'./images/Espejito2500.png',
+			'./images/Paula.png',
+			'./images/Wheelook.png',
+			'./images/MarioCabrera.png',
+			'./images/Paticama.png',
+			'./images/Vinci.png'
+		]
+		const images = imagesArray[Math.floor(Math.random() * imagesArray.length)];
+
+		await ctx.reply({content: 'A', files: [images]});
 	},
 });
