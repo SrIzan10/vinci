14126d0bdb1b9b2f45dfb71aaf3dfefaf039038d
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2022-08-24 10:56:08 +0200

    fix: radio not being able to use by the members

--- commands/misc/playRadio.ts
+++ commands/misc/playRadio.ts
@@ -13,8 +13,8 @@ import { ownerOnly } from "../../src/plugins/ownerOnly"
 export default commandModule({
 	name: 'radio',
     type: CommandType.Both,
-	plugins: [publish({ guildIds: ['1000400148289036298', '928018226330337280'] }), ownerOnly()],
-	description: 'ADMIN: Reproduce la radio', 
+	plugins: [publish({ guildIds: ['1000400148289036298', '928018226330337280'] })],
+	description: 'Reproduce la radio', 
 	options: [
 		{
 			name: 'reproducir',
