b8a867ae3805ed89d0074b269cb6b2bd8858e686
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2022-08-31 18:23:44 +0200

    feat: Menu for selecting roles!

---
+++ commands/misc/role-menu.ts
@@ -0,0 +1,34 @@
+import { commandModule, CommandType } from "@sern/handler";
+import type { APISelectMenuComponent, GuildMember } from "discord.js";
+
+export default commandModule({
+	type: CommandType.MenuSelect,
+    name: 'role-menu',
+	async execute(interaction) {
+		await interaction.deferReply({ ephemeral: true });
+
+		const roles = interaction.values;
+
+		const menuRoles: string[] = (
+			interaction.message.components[0].components[0]
+				.data as Readonly<APISelectMenuComponent>
+		).options.map((o: { label: string; value: string }) => o.value);
+
+		const member = interaction.member as GuildMember;
+		if (!member) return;
+
+		let content = `Los roles han sido actualizados. Te he dado estos::\n${roles
+			.map((r) => `<@&${r}>`)
+			.join("\n")}`;
+		if (roles.length === 0) content = "No se han seleccionado roles...";
+
+		const existing = member.roles.cache
+			.filter((r) => r.id !== interaction.guildId)
+			.map((r) => r.id)
+			.filter((r) => !menuRoles.includes(r));
+
+		await member.roles.set(roles.concat(existing)).catch(() => null);
+
+		await interaction.editReply(content);
+	},
+});
---
+++ commands/misc/roleMenu.ts
@@ -0,0 +1,85 @@
+const { commandModule, CommandType } = require('@sern/handler');
+import { publish } from "../../src/plugins/publish";
+import { ownerOnly } from "../../src/plugins/ownerOnly"
+import { ActionRowBuilder, ApplicationCommandOptionType, ChannelType, Collection, EmbedBuilder, Role, SelectMenuBuilder, TextChannel } from "discord.js";
+import { Resolver } from "../../resolver";
+
+export default commandModule({
+	name: 'rolemenu',
+    type: CommandType.Both,
+	plugins: [publish({ guildIds: ['1000400148289036298', '928018226330337280'] }), ownerOnly()],
+	description: 'ADMIN: Spawnea un menú de roles',
+	//alias : [],
+	options: [
+		{
+			name: "channel",
+			type: ApplicationCommandOptionType.Channel,
+			description: "The channel to send the message to",
+			channelTypes: [ChannelType.GuildText],
+			required: true,
+		},
+		{
+			name: "role",
+			type: ApplicationCommandOptionType.String,
+			description: "The roles to attach (upto 25)",
+			required: true,
+		},
+		{
+			name: "message",
+			type: ApplicationCommandOptionType.String,
+			description: "The message to send",
+			required: true,
+		},
+	],
+	execute: async (ctx, options) => {
+		const channel = options[1].getChannel("channel", true) as TextChannel;
+		const role = new Resolver(options[1].getString("role", true), ctx.interaction)
+			.roles;
+		const message = options[1].getString("message", true);
+
+		if (role.size > 25) return ctx.reply("Too many roles");
+
+		const cdn = role.filter(
+			(r) =>
+				r.managed ||
+				r.position > (ctx.guild?.members.me)!.roles.highest.position
+		).size;
+		if (cdn) {
+			return ctx.reply(
+				`Some roles are managed by integration or higher than my highest role.\nPlease try again`
+			);
+		}
+		await ctx.interaction.deferReply();
+		const row = createMenu(channel, role);
+		const embed = new EmbedBuilder()
+			.setTitle(message)
+			.setDescription(
+				`Please select your roles below\nYou can select multiple roles`
+			)
+			.setColor(0xcc5279);
+		await channel.send({
+			embeds: [embed],
+			components: [row],
+		});
+		await ctx.interaction.editReply("✅ Done!");
+	},
+});
+
+function createMenu(channel: TextChannel, role: Collection<string, Role>) {
+	if (!channel || !role) throw new Error("Missing channel or role");
+	const menu = new SelectMenuBuilder()
+		.setCustomId("role-menu")
+		.setMaxValues(role.size)
+		.setMinValues(0)
+		.setPlaceholder("Pick some roles here!")
+		.setOptions(
+			role.map((r) => {
+				return {
+					label: r.name,
+					value: r.id,
+				};
+			})
+		);
+	const row = new ActionRowBuilder<SelectMenuBuilder>().setComponents(menu);
+	return row;
+};
