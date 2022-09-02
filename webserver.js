c4d1f95040c226da6371b4513f9d1570dd8630b0
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2022-09-02 18:42:15 +0200

    feat: raspberry pi monitoring

--- webserver.js
+++ webserver.js
@@ -6,9 +6,9 @@ app.use(express.static("public"))
 
 // define the first route
 app.get("/", function (req, res) {
-  res.send("<p>This is the first server for the Vinci discord bot!</p>")
+  res.send("<p>This is the monitoring server for the Vinci discord bot!</p><br><p>If you see this, the bot is up and running.</p>")
 })
 
 // start the server listening for requests
-app.listen(process.env.PORT || 3000, 
+app.listen(process.env.PORT || 7272,
 	() => console.log("The webserver is listening on port " + process.env.PORT || 3000));
