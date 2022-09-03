0ed38b81c6e9f294d5432169f009d6fad8cc0ce8
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2022-09-03 22:19:44 +0200

    fix: special characters and limits

--- commands/minecraft/mcform.ts
+++ commands/minecraft/mcform.ts
@@ -8,13 +8,18 @@ export default commandModule({
 	//alias : [],
 	async execute (modal) {
 		const value = modal.fields.getTextInputValue('mcUsernameInput');
-		try {
-			const request = await axios(`https://api.mojang.com/users/profiles/minecraft/${value}`, {validateStatus: function (status) {return status === 200 || status === 400; }})
-			const data = request.data
-			await modal.reply({content: 'Enviado!, Gracias por utilizar tu Mona Lisa de confianza\n~Sr Izan, 2022', ephemeral: true})
-			modal.client.guilds.cache.get("928018226330337280").channels.cache.get("998195363376803850").send(`Solicitud enviada por ${modal.user}.\nUsername de Minecraft: ${value}`);
-		} catch (err) {
-			await modal.reply({content: 'ERROR: No se ha podido enviar ya que eres un usuario no premium o de MC Bedrock.\nAsegúrate que has puesto bien el nombre de usuario.', ephemeral: true})
-		}
+		var specialChars = /[`!@#$%^&*()+\-=\[\]{};':"\\|,.<>\/?~]/;
+		if (value > 16 || value <= 2 || specialChars.test(value)) {
+			modal.reply({content: `ERROR: El nombre de usuario no es válido.`, ephemeral: true});
+		} else {
+			try {
+				const request = await axios(`https://api.mojang.com/users/profiles/minecraft/${value}`, {validateStatus: function (status) {return status === 200 || status === 400; }})
+				const data = request.data
+				await modal.reply({content: 'Enviado!, Gracias por utilizar tu Mona Lisa de confianza\n~Sr Izan, 2022', ephemeral: true})
+				modal.client.guilds.cache.get("928018226330337280").channels.cache.get("998195363376803850").send(`Solicitud enviada por ${modal.user}.\nUsername de Minecraft: ${value}`);
+			} catch (err) {
+				await modal.reply({content: 'ERROR: No se ha podido enviar ya que eres un usuario no premium o de MC Bedrock.\nAsegúrate que has puesto bien el nombre de usuario.', ephemeral: true})
+			}
+	}
 	}
 });
