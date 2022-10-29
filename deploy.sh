682685899f5759a310ee34edada26ef0483468b2
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2022-10-29 23:34:41 +0200

    revert: i quit

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,10 @@
-doallstuff () {
-    git pull
+#!/bin/bash
+git pull
 
-    docker build . -t srizan10/vinci
+docker build . -t srizan10/vinci
 
-    docker stop vinci
+docker stop vinci
 
-    docker rm vinci
+docker rm vinci
 
-    docker run -d -t --name vinci -p 7272:7272 --restart unless-stopped srizan10/vinci
-}
-
-if doallstuff()
-then env GITHUB_API=$COMMITSTATUS commit-status success deployment/rpi "Deployment succesful!"
-else env GITHUB_API=$COMMITSTATUS commit-status failure deployment/rpi "Oops! Something went wrong!"
+docker run -d -t --name vinci -p 7272:7272 --restart unless-stopped srizan10/vinci
