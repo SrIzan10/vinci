41aefe2012550d0ca2ceb94066ea9b2a1e98e0ff
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2022-11-05 13:34:36 +0100

    feat: cursivify messages

---
+++ commands/fun/cursivify.ts
@@ -0,0 +1,24 @@
+import { commandModule, CommandType } from '@sern/handler'
+/*
+import { publish } from "../../src/plugins/publish.js";
+import { ownerOnly } from "../../src/plugins/ownerOnly.js"
+*/
+
+export default commandModule({
+	name: 'cursivify',
+    type: CommandType.Text,
+	plugins: [],
+	// , '928018226330337280'
+	description: 'Haz un mensaje en *cursiva*',
+	alias : ['cu'],
+	execute: async (ctx, options) => {
+		try {
+			const repliedmessage = await ctx.message.channel.messages.fetch(ctx.message.reference!.messageId!);
+			const trimmedstring = repliedmessage.content.replaceAll('*', '')
+			await ctx.reply(`*${trimmedstring}*`)
+		} catch (err) {
+			await ctx.reply('Asegúrate que estás respondiendo al mensaje que quieras hacer cursiva!')
+		}
+		
+	},
+});
