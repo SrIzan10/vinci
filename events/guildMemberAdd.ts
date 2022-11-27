3926fd31cc2c53ddc7c265ffc25b39c37c35dc55
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2022-11-27 19:40:56 +0100

    fix: guildmemberadd and leave issuing on test srv

--- events/guildMemberAdd.ts
+++ events/guildMemberAdd.ts
@@ -6,14 +6,15 @@ export default eventModule({
   type: EventType.Discord,
   name: 'guildMemberAdd',
   execute(member: GuildMember) {
-		const newMemberEmbed = new EmbedBuilder()
-            .setColor("Random")
-            .setTitle("Nuevo miembro!")
-            .setDescription(`${member.user} acaba de entrar al servidor!`)
-            .setThumbnail(member.user.displayAvatarURL())
-            .setTimestamp();
+    if (member.guild.id !== process.env.GUILDID) return;
+	const newMemberEmbed = new EmbedBuilder()
+        .setColor("Random")
+        .setTitle("Nuevo miembro!")
+        .setDescription(`${member.user} acaba de entrar al servidor!`)
+        .setThumbnail(member.user.displayAvatarURL())
+        .setTimestamp();
 
-        const channel = member.client.guilds.cache.get(process.env.GUILDID!)!.channels.cache.get(process.env.JOINSANDLEAVES_CHANNEL!) as TextChannel
-        channel.send({embeds: [newMemberEmbed]})
+    const channel = member.client.guilds.cache.get(process.env.GUILDID!)!.channels.cache.get(process.env.JOINSANDLEAVES_CHANNEL!) as TextChannel
+    channel.send({embeds: [newMemberEmbed]})
     }
 });
