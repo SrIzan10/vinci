4fcded099d30c8944cb85124341a1a29ac4399fc
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2022-11-27 19:43:43 +0100

    fix: also guildmemberremove

--- events/guildMemberRemove.ts
+++ events/guildMemberRemove.ts
@@ -6,15 +6,15 @@ export default eventModule({
   type: EventType.Discord,
   name: 'guildMemberRemove',
   execute(member: GuildMember) {
-    	// member.guild.channels.cache.get("968572106952560670").send(`${member.user} has joined the server!`);
-		const leaveEmbed = new EmbedBuilder()
+    if (member.guild.id !== process.env.GUILDID) return;
+	const leaveEmbed = new EmbedBuilder()
         .setColor("Random")
         .setTitle("Un miembro se ha ido :(")
         .setDescription(`${member.user} acaba de salir del servidor!`)
         .setThumbnail(member.user.displayAvatarURL())
         .setTimestamp();
 
-        const channel = member.client.guilds.cache.get(process.env.GUILDID!)!.channels.cache.get(process.env.JOINSANDLEAVES_CHANNEL!) as TextChannel
-        channel.send({embeds: [leaveEmbed]})
+    const channel = member.client.guilds.cache.get(process.env.GUILDID!)!.channels.cache.get(process.env.JOINSANDLEAVES_CHANNEL!) as TextChannel
+    channel.send({embeds: [leaveEmbed]})
     }
 });
