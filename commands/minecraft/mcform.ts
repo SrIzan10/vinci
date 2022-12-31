27f1aa565472c20e2927edd5f651950aef88a22d
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2022-12-31 20:53:54 +0100

    fix: mcform not sending

--- commands/minecraft/mcform.ts
+++ commands/minecraft/mcform.ts
@@ -1,32 +1,33 @@
-import { commandModule, CommandType } from '@sern/handler'
-import { ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, ModalActionRowComponentBuilder } from 'discord.js'
-import { publish } from "#plugins";
-import { ownerOnly } from "#plugins";
-
+import { commandModule, CommandType } from '@sern/handler';
+import {
+	ActionRowBuilder,
+	ModalBuilder,
+	TextInputBuilder,
+	TextInputStyle,
+	ModalActionRowComponentBuilder,
+} from 'discord.js';
+import { publish } from '#plugins';
+import { ownerOnly } from '#plugins';
 
 export default commandModule({
 	name: 'mcform',
-    type: CommandType.Slash,
+	type: CommandType.Slash,
 	plugins: [publish()],
 	description: 'Envia el formulario para entrar al servidor.',
 	//alias : [],
 	execute: async (ctx) => {
 		const modal = new ModalBuilder()
-		.setCustomId('mcform')
-		.setTitle('Formulario para entrar al servidor');
-				// Create the text input components
-				const input = new TextInputBuilder()
-					.setCustomId('mcUsernameInput')
-					// The label is the prompt the user sees for this input
-					.setLabel("Cuál es tu nombre de usuario de Minecraft?")
-					// Short means only a single line of text
-					.setStyle(TextInputStyle.Short);
-			// An action row only holds one text input,
-			// so you need one action row per text input.
-			const usernameActionRow = new ActionRowBuilder<ModalActionRowComponentBuilder>().addComponents(input);
-	
-			// Add inputs to the modal
-			modal.addComponents(usernameActionRow);
-			await ctx.interaction.showModal(modal);
-	}
+			.setCustomId('mcform-main')
+			.setTitle('Formulario para entrar al servidor');
+		const input = new TextInputBuilder()
+			.setCustomId('mcUsernameInput')
+			.setLabel('Cuál es tu nombre de usuario de Minecraft?')
+			.setStyle(TextInputStyle.Short);
+		const usernameActionRow =
+			new ActionRowBuilder<ModalActionRowComponentBuilder>().addComponents(
+				input
+			);
+		modal.addComponents(usernameActionRow);
+		await ctx.interaction.showModal(modal);
+	},
 });
