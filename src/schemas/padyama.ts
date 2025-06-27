459c6f9892d6c0fe634f33767d5da15632807e40
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2025-06-27 17:09:04 +0200

    feat: almost blank slate

--- src/schemas/padyama.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-const schema = new mongoose.Schema({ 
-	id: {type: String, required: true},
-	user: {type: String, required: true},
-	suggestionid: {type: String, required: true},
-	suggestion: {type: String, required: true},
-});
-const db = mongoose.model('padyama', schema, 'padyama');
-export default db
